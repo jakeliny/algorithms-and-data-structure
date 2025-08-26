7e4e436212bc267973eb55586c8caef29d5f1298
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2025-08-26 18:04:39 -0300

    feat: improve quick sort with random pivo

--- ts/src/quick-sort/index.ts
+++ ts/src/quick-sort/index.ts
@@ -3,12 +3,15 @@ export function quickSort(array: number[]): number[] {
 		return array;
 	}
 
-	const pivo = array.shift();
+	const randomIndex = Math.floor(Math.random() * array.length);
+	const pivo = array[randomIndex];
+	array.splice(randomIndex, 1);
+
 	const smallest: number[] = [];
 	const biggest: number[] = [];
 
 	for (const item of array) {
-		if (item < pivo!) {
+		if (item < pivo) {
 			smallest.push(item);
 		} else {
 			biggest.push(item);
@@ -16,20 +19,4 @@ export function quickSort(array: number[]): number[] {
 	}
 
 	return [...quickSort(smallest), pivo, ...quickSort(biggest)];
-}
-
-export function quickSortWithFilter(array: number[]): number[] {
-	if (array.length < 2) {
-		return array;
-	}
-
-	const pivo = array.shift();
-	const smallest: number[] = array.filter((n) => n < pivo);
-	const biggest: number[] = array.filter((n) => n > pivo);
-
-	return [
-		...quickSortWithFilter(smallest),
-		pivo,
-		...quickSortWithFilter(biggest),
-	];
 }
